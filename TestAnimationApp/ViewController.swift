400c97da262c3149b7e8360b6e613fab15c88be6
outsider1703 <simka-simka020697simka-simka1>
2020-06-17 01:04:16 +0200

    Initial Commit

---
+++ TestAnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAnimationApp
+//
+//  Created by Macbook on 17.06.2020.
+//  Copyright © 2020 Igor Simonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
