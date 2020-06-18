c58a87d86f21e9b85d851a0c5a96b7fd1c121d5f
outsider1703 <simka-simka020697simka-simka1>
2020-06-18 13:26:42 +0200

    HW 2.9

--- TestAnimationApp/ViewController.swift
+++ TestAnimationApp/ViewController.swift
@@ -47,7 +47,7 @@ class ViewController: UIViewController {
             }
         }
     }
-}
+
 
 extension ViewController {
     func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
